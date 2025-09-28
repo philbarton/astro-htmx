de5557cd33772c0f1a70d2eb23b6c8b0d54c08d2
philbarton <GRwWYPae9dCpzL>
2025-09-28 21:35:03 +0100

    bind

--- src/pages/api/show-key.js
+++ src/pages/api/show-key.js
@@ -1,15 +1,20 @@
 // src/pages/api/check-cookie.js
 export const prerender = false;
 
-export async function GET({cookies}) {
+export async function GET({cookies, env}) {
     const cookie = cookies.get("user")?.value || "";
 
     const secret = import.meta.env.MY_API_KEY;
     const variable = import.meta.env.MY_VARIABLE;
     let mode = import.meta.env.MODE;
-
+    let response;
+    if (env?.MY_WORKER) {
+        // Cloudflare dev/prod
+        response = await env.MY_WORKER.fetch("https://dummy/");
+    }
+    const text = await response?.text();
     return new Response(
-        `<p>${variable}</p><p>${secret}</p><p>${cookie}</p><p>${mode}</p>`,
+        `<p>${variable}</p><p>${secret}</p><p>${cookie}</p><p>${mode}</p><p>${text}</p>`,
         { headers: { "Content-Type": "text/html" } }
     );
 }
