ef50b5e991e8f5e368390d8c08da4ac3732ce5b4
philbarton <GRwWYPae9dCpzL>
2025-09-28 20:08:12 +0100

    toml

--- src/pages/api/show-key.js
+++ src/pages/api/show-key.js
@@ -3,10 +3,11 @@ export const prerender = false;
 
 export async function GET() {
 
+    const secret = import.meta.env.MY_API_KEY;
     const variable = import.meta.env.MY_VARIABLE;
 
     return new Response(
-        `<p>${variable}</p>`,
+        `<p>${variable}</p><p>${secret}</p>`,
         { headers: { "Content-Type": "text/html" } }
     );
 }
